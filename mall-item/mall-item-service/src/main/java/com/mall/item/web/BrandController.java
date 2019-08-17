fd2abefec744c3a0c2e95d132a4794c095b4e1df
Administrator <2084267015@qq.com>
2019-08-17 23:23:44 +0800

    page

--- mall-item/mall-item-service/src/main/java/com/mall/item/web/BrandController.java
+++ mall-item/mall-item-service/src/main/java/com/mall/item/web/BrandController.java
@@ -17,13 +17,6 @@ import java.util.List;
 /**
  * 网关访问：/api/item是前缀
  *
- * ajax请求不允许跨域，出于安全考虑：cookie登陆信息
- * 解决方法：
- *1. jsonp: 需要服务器的支持，只支持get请求
- * 2. cors: 支持各种请求；在服务端控制
- * 浏览器把ajax分为：简单请求；特殊请求
- * 简单请求：请求头携带origin：当前网站域名--后台服务器：决定是否跨域--
- * 允许：响应头：包含当前网站域名+access control allow credential
  */
 @RestController
 @RequestMapping("brand")
@@ -33,7 +26,7 @@ public class BrandController {
     private BrandService brandService;
 
     @GetMapping("page")
-    public ResponseEntity<PageResult<Brand>> getListByPid(
+    public ResponseEntity<PageResult<Brand>> getListByPage(
             @RequestParam(value = "page", defaultValue = "1") Integer page,
             @RequestParam(value = "rows", defaultValue = "5") Integer rows,
             @RequestParam(value = "sortedBy", required = false) String sortedBy,
