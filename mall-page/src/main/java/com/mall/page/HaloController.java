6a003cbc98b352e928aa38367fb99201d851b740
Administrator <2084267015@qq.com>
2019-08-25 13:39:21 +0800

    html

--- mall-page/src/main/java/com/mall/page/HaloController.java
+++ mall-page/src/main/java/com/mall/page/HaloController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 @Controller
 public class HaloController {
 
@@ -20,9 +22,11 @@ public class HaloController {
         user.setName("xi");
         user.setAge(12);
         user.setFriend(new User("li", 11, null));
-
+        User user1 = new User("tg", 33, null);
 
         model.addAttribute("user", user);
+        model.addAttribute("users", Arrays.asList(user, user1));
         return "halo";//view
     }
+
 }
