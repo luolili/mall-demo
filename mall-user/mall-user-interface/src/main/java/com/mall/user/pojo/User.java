0096adc650b552952b84cca473cf56eb84389716
Administrator <2084267015@qq.com>
2019-08-31 21:01:36 +0800

    mall user

--- mall-user/mall-user-interface/src/main/java/com/mall/user/pojo/User.java
+++ mall-user/mall-user-interface/src/main/java/com/mall/user/pojo/User.java
@@ -6,6 +6,8 @@ import tk.mybatis.mapper.annotation.KeySql;
 
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Table
@@ -14,9 +16,13 @@ public class User {
     @Id
     @KeySql(useGeneratedKeys = true)
     private Long id;
+    @NotEmpty
     private String username;
     @JsonIgnore
     private String password;//加密后save
+    @Pattern(regexp =
+            "^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\\\d{8}$"
+            , message = "手机号不正确")
     private String phone;
     private Date created;
     @JsonIgnore
--- mall-user/mall-user-service/src/main/java/com/mall/user/web/UserController.java
+++ mall-user/mall-user-service/src/main/java/com/mall/user/web/UserController.java
@@ -3,9 +3,14 @@ package com.mall.user.web;
 import com.mall.user.pojo.User;
 import com.mall.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 @RestController
 public class UserController {
@@ -27,7 +32,14 @@ public class UserController {
     }
 
     @PostMapping("code")
-    public ResponseEntity<Void> register(User user, @RequestParam("code") String code) {
+    public ResponseEntity<Void> register(@Valid User user,
+                                         BindingResult result,
+                                         @RequestParam("code") String code) {
+        if (result.hasErrors()) {
+            throw new RuntimeException(result.getFieldErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.joining("|")));
+        }
         userService.register(user, code);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
